14c716a4bc736a2b4e783996f5f66299e13e713b
Dmitry V. Levin <ldv@altlinux.org>
2004-11-15 15:43:28 +0000

    optimized

--- etc/profile_d/xhost.sh
+++ etc/profile_d/xhost.sh
@@ -1,6 +1,6 @@
 # "$Id"
 # Export Xauthority for non-root users.
 
-if [ -z "$XAUTHORITY" -a -z "$SSH_TTY" -a ! -z "$DISPLAY" -a "$UID" -gt 14 ]; then
+if [ -z "$XAUTHORITY" -a -z "$SSH_TTY" -a -n "$DISPLAY" -a "$UID" -gt 14 ]; then
 	export XAUTHORITY="$HOME/.Xauthority"
 fi
